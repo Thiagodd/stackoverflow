b4e802392886273e3b209f06ab84a28e3217ff14
Thiago Silva <thiagodd.dev@gmail.com>
2024-02-16 14:40:47 -0300

    feat: create service layer for tag

---
+++ src/main/java/com/thiagodd/stackoverclone/api/controller/ITagController.java
@@ -0,0 +1,7 @@
+package com.thiagodd.stackoverclone.api.controller;
+
+import com.thiagodd.stackoverclone.domain.dto.TagDTO;
+import com.thiagodd.stackoverclone.domain.model.Tag;
+
+public interface ITagController extends ICrudController<Tag, TagDTO>{
+}
---
+++ src/main/java/com/thiagodd/stackoverclone/domain/service/ITagService.java
@@ -0,0 +1,7 @@
+package com.thiagodd.stackoverclone.domain.service;
+
+import com.thiagodd.stackoverclone.domain.dto.TagDTO;
+import com.thiagodd.stackoverclone.domain.model.Tag;
+
+public interface ITagService extends ICrudService<Tag, TagDTO> {
+}
